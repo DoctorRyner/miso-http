f1f6b298e5d6f86db39f59e74400cc0d123a3ee7
Ryner Gabriel <drryner@icloud.com>
2019-10-29 21:49:42 +0300

    Some changes for POST request

--- src/Miso/Http.hs
+++ src/Miso/Http.hs
@@ -46,9 +46,8 @@ getLocalFile :: MisoString -> JSM (Response MisoString)
 getLocalFile path = Miso.Http.sendPlain $ get { url = path, headers = [] }
 
 send :: (FromJSON response, ToJSON response, ToJSON payload) => Request payload -> JSM (Response response)
-send = \case
+send r = newXMLHttpRequest >>= \req -> case r of
     GET url headers -> do
-        req <- newXMLHttpRequest
         openSimple req ("GET" :: MisoString) url
         mapM_ (uncurry $ setRequestHeader req) headers
         eitherXhrError <- try $ JSDOM.send req
@@ -66,7 +65,6 @@ send = \case
                     Left err  -> pure $ HttpError (ms err) (fromEnum statusNum)
             Left (SomeException _) -> pure $ HttpError "XHRError" 404
     POST url headers maybePayload -> do
-        req <- newXMLHttpRequest
         openSimple req ("POST" :: MisoString) url
         mapM_ (uncurry $ setRequestHeader req) headers
         eitherXhrError <- try $ case maybePayload of
@@ -74,7 +72,7 @@ send = \case
             Nothing      -> JSDOM.send req
         case eitherXhrError of
             Right _ -> do
-                resRaw <- valToStr =<< getResponse req
+                resRaw <- valToJSON =<< getResponse req
                 statusNum <- fromEnum <$> getStatus req
                 let resBS = TLEncoding.encodeUtf8 $ TL.pack $ unpack resRaw
                 case eitherDecode resBS of
